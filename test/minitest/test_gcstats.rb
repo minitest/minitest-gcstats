e101d6eeaaa566c3c4161a25400e09ce458aa4b6
Ryan Davis <ryand@zenspider.com>
2014-08-29 16:26:45 -0800

    Add tests to demo [git-p4: depot-paths = "//src/minitest-gcstats/dev/": change = 9725]

--- test/minitest/test_gcstats.rb
+++ test/minitest/test_gcstats.rb
@@ -4,7 +4,11 @@ require "minitest/gcstats"
 module TestMinitest; end
 
 class TestMinitest::TestGcstats < Minitest::Test
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def test_empty
+    # do nothing
+  end
+
+  def test_full
+    Object.new
   end
 end
