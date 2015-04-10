689a434b156efe1d642b4647d4ca782c5417e8c2
Ryan Davis <ryand@zenspider.com>
2015-04-10 15:43:28 -0800

    quelled warnings [git-p4: depot-paths = "//src/minitest-gcstats/dev/": change = 10114]

--- test/minitest/test_gcstats.rb
+++ test/minitest/test_gcstats.rb
@@ -369,13 +369,13 @@ class TestAssertObject < Minitest::Test
   end
 
   def test_assert_allocations_neg_lt
-    assert_allocations -3 do
+    assert_allocations(-3) do
       2.times { Object.new }
     end
   end
 
   def test_assert_allocations_neg_eq
-    assert_allocations -3 do
+    assert_allocations(-3) do
       3.times { Object.new }
     end
   end
@@ -406,7 +406,7 @@ class TestAssertObject < Minitest::Test
     exp = "Object allocations.\nExpected 5 to be <= 3."
 
     assert_triggered exp do
-      assert_allocations -3 do
+      assert_allocations(-3) do
         5.times { Object.new }
       end
     end
